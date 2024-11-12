52dbe8281233e1c138e31d9617ca72259a5227b9
SM <52240990+baesumin@users.noreply.github.com>
2024-11-13 00:30:28 +0900

    .

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -123,10 +123,10 @@ export const highlightPattern = (text: string, pattern: string) => {
     pattern,
     (value) =>
       `<span style="
-        background-color: rgba(255, 255, 0, 0.7);
+        background-color: rgba(255, 255, 0, 1);
         border-radius:200px;
         filter:blur(100px);
-        box-shadow: 0 0 200px rgba(255, 255, 0, 0.7);
+        box-shadow: 0 0 200px rgba(255, 255, 0, 1);
       ">${value}</span>`
   );
 };
