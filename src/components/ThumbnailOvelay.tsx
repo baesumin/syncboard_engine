999f64cf0536b5652f060a66a5c5e409a8390276
SM <52240990+baesumin@users.noreply.github.com>
2024-11-27 09:33:19 +0900

    fix: 썸네일 높이 설정

--- src/components/ThumbnailOvelay.tsx
+++ src/components/ThumbnailOvelay.tsx
@@ -4,8 +4,12 @@ import { Thumbnail } from "react-pdf";
 import { useAtom } from "jotai";
 import { pdfStateAtom } from "../store/pdf";
 import { PathsType } from "../types/common";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { reDrawPathGroup } from "../utils/common";
+import {
+  OnItemClickArgs,
+  OnPageLoadSuccess,
+} from "react-pdf/src/shared/types.js";
 
 const ThumbnailOvelay = ({
   paths,
@@ -15,85 +19,108 @@ const ThumbnailOvelay = ({
   };
 }) => {
   const canvasRefs = useRef<(HTMLCanvasElement | null)[]>([]);
+  const [thumbnailHeight, setThumbnailHeight] = useState(0);
   const [pdfState, setPdfState] = useAtom(pdfStateAtom);
+
+  const setRef = useCallback((node: HTMLCanvasElement) => {
+    if (node) {
+      const indexValue = Number(node.getAttribute("data-index"));
+      canvasRefs.current[indexValue] = node;
+    }
+  }, []);
+
+  const onLoadSuccess: OnPageLoadSuccess = useCallback(
+    (page) => {
+      if (!thumbnailHeight) {
+        setThumbnailHeight(page.height);
+      }
+    },
+    [thumbnailHeight]
+  );
+
+  const onItemClick = useCallback(
+    (args: OnItemClickArgs) => {
+      setPdfState({
+        ...pdfState,
+        pageNumber: args.pageNumber,
+        isListOpen: false,
+      });
+    },
+    [pdfState, setPdfState]
+  );
 
   const redrawPaths = useCallback(
     (pageNumber: number) => {
       if (!canvasRefs.current[pageNumber] || !paths[pageNumber]) return;
-      const matches =
-        canvasRefs.current[pageNumber].style.height.match(/[\d.]+/);
-      if (matches) {
-        canvasRefs.current[pageNumber].width = 180 * 2;
-        canvasRefs.current[pageNumber].height = parseFloat(matches[0]) * 2;
-        const points = paths[pageNumber];
-        if (!points || points.length === 0) return;
-        const context = canvasRefs.current[pageNumber].getContext("2d")!;
-        context.clearRect(
-          0,
-          0,
-          canvasRefs.current[pageNumber].width,
-          canvasRefs.current[pageNumber].height
-        );
-        let currentGroup: PathsType[] = [];
+      canvasRefs.current[pageNumber].width = 180 * 2;
+      canvasRefs.current[pageNumber].height = thumbnailHeight * 2;
+      const points = paths[pageNumber];
+      if (!points || points.length === 0) return;
+      const context = canvasRefs.current[pageNumber].getContext("2d")!;
+      context.clearRect(
+        0,
+        0,
+        canvasRefs.current[pageNumber].width,
+        canvasRefs.current[pageNumber].height
+      );
+      let currentGroup: PathsType[] = [];
 
-        if (points.length === 1) return;
+      if (points.length === 1) return;
 
-        let currentStyle = {
-          color: points[1].color,
-          lineWidth: points[1].lineWidth,
-          alpha: points[1].alpha,
-        };
+      let currentStyle = {
+        color: points[1].color,
+        lineWidth: points[1].lineWidth,
+        alpha: points[1].alpha,
+      };
 
-        for (let i = 1; i < points.length; i++) {
-          // 선이 이어진 경우
-          if (
-            points[i].lastX === points[i - 1].x &&
-            points[i].lastY === points[i - 1].y
-          ) {
-            if (i === 1) currentGroup.push(points[0]);
-            currentGroup.push(points[i]);
-            continue;
-          }
-
-          // 선이 띄워진 경우
-          if (currentGroup.length) {
-            context.beginPath();
-
-            // 현재 그룹이 2개 이상의 점을 포함하면 선 그리기
-            reDrawPathGroup(
-              context,
-              currentGroup,
-              currentStyle,
-              180,
-              parseFloat(matches[0])
-            );
-          }
-
-          // 단일 점 처리
-          currentGroup = [points[i]]; // 새로운 그룹 초기화
-          currentStyle = {
-            color: points[i].color,
-            lineWidth: points[i].lineWidth,
-            alpha: points[i].alpha,
-          };
+      for (let i = 1; i < points.length; i++) {
+        // 선이 이어진 경우
+        if (
+          points[i].lastX === points[i - 1].x &&
+          points[i].lastY === points[i - 1].y
+        ) {
+          if (i === 1) currentGroup.push(points[0]);
+          currentGroup.push(points[i]);
+          continue;
         }
-        // 마지막 그룹 처리
+
+        // 선이 띄워진 경우
         if (currentGroup.length) {
           context.beginPath();
 
+          // 현재 그룹이 2개 이상의 점을 포함하면 선 그리기
           reDrawPathGroup(
             context,
             currentGroup,
             currentStyle,
             180,
-            parseFloat(matches[0])
+            thumbnailHeight
           );
         }
+
+        // 단일 점 처리
+        currentGroup = [points[i]]; // 새로운 그룹 초기화
+        currentStyle = {
+          color: points[i].color,
+          lineWidth: points[i].lineWidth,
+          alpha: points[i].alpha,
+        };
+      }
+      // 마지막 그룹 처리
+      if (currentGroup.length) {
+        context.beginPath();
+
+        reDrawPathGroup(
+          context,
+          currentGroup,
+          currentStyle,
+          180,
+          thumbnailHeight
+        );
       }
     },
-    [paths]
+    [thumbnailHeight, paths]
   );
-
   useEffect(() => {
     if (pdfState.isListOpen) {
       Object.keys(paths)
@@ -138,32 +165,27 @@ const ThumbnailOvelay = ({
                   pdfState.pageNumber === index + 1
                     ? "border-[3px] border-[#FF9A51]"
                     : "",
-                  "overflow-hidden h-[232px] relative"
+                  "overflow-hidden relative"
                 )}
+                style={{ height: thumbnailHeight }}
               >
                 <Thumbnail
                   pageNumber={index + 1}
                   width={180}
                   devicePixelRatio={2}
-                  onItemClick={({ pageNumber }) => {
-                    setPdfState({
-                      ...pdfState,
-                      pageNumber: pageNumber,
-                      isListOpen: false,
-                    });
-                  }}
+                  onItemClick={onItemClick}
+                  onLoadSuccess={onLoadSuccess}
                   loading={<></>}
                   className="absolute"
                 />
                 <canvas
-                  ref={(el) => {
-                    canvasRefs.current[index + 1] = el;
-                  }}
+                  ref={setRef}
                   style={{
                     width: 180,
-                    height: 232,
+                    height: thumbnailHeight,
                   }}
                   className="absolute pointer-events-none"
+                  data-index={index + 1}
                 />
               </div>
               <div className="h-[31px] flex justify-center items-center">
