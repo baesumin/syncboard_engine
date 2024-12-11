a0dc4cb55c8b089b14c1f9a213fc562ecf5af33b
SM <52240990+baesumin@users.noreply.github.com>
2024-12-11 11:09:20 +0900

    .

--- src/PdfEngine.tsx
+++ src/PdfEngine.tsx
@@ -108,6 +108,8 @@ export default function PdfEngine() {
   useWebviewInterface({
     paths,
     getSearchResult,
+    scaleRef,
+    canvasRefs,
   });
 
   const OnPageLoadSuccess: OnPageLoadSuccess = useCallback(
