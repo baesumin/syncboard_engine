38f63796099d8540ca6bd14c7dfd9a8927750d1a
SM <52240990+baesumin@users.noreply.github.com>
2025-02-03 15:40:29 +0900

    bump: tailwind 4버전 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from "@tailwindcss/vite";
 
 const require = createRequire(import.meta.url);
 
@@ -24,5 +25,6 @@ export default defineConfig({
         },
       ],
     }),
+    tailwindcss(),
   ],
 });
